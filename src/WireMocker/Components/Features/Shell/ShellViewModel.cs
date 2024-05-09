c6cf204a02b7e2e9eb990500e9233d0706c6c3f4
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-05-09 08:23:22 +0700

    Use SectionOutlet instead of title injection hack

--- src/WireMocker/Components/Features/Shell/ShellViewModel.cs
+++ src/WireMocker/Components/Features/Shell/ShellViewModel.cs
@@ -1,26 +1,14 @@
-﻿using Microsoft.AspNetCore.Components;
-using ReactiveUI;
+﻿using ReactiveUI;
 
 namespace Tirax.Application.WireMocker.Components.Features.Shell;
 
 public sealed class ShellViewModel : ViewModel
 {
     bool isDrawerOpen = true;
-    RenderFragment? header;
 
     public bool IsDrawerOpen
     {
         get => isDrawerOpen;
         set => this.RaiseAndSetIfChanged(ref isDrawerOpen, value);
-    }
-
-    public RenderFragment? Header
-    {
-        get => header;
-        set => this.RaiseAndSetIfChanged(ref header, value);
-    }
-
-    public void SetHeader(string text) {
-        Header = builder => builder.AddContent(0, text);
     }
 }
