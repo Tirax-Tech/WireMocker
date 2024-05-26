7f331e68999357dcc1bf979c41690db69b24513e
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-05-26 17:05:34 +0700

    Implement load data / new data into a service list

--- src/WireMocker/Components/Features/DesignServices/SearchPanelViewModel.cs
+++ src/WireMocker/Components/Features/DesignServices/SearchPanelViewModel.cs
@@ -1,31 +1,64 @@
-﻿using System.Reactive.Concurrency;
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using DynamicData;
+using LanguageExt.Common;
+using MudBlazor;
 using ReactiveUI;
+using RZ.Foundation;
+using RZ.Foundation.Extensions;
 using Tirax.Application.WireMocker.Components.Features.Shell;
+using Tirax.Application.WireMocker.Domain;
 using Tirax.Application.WireMocker.Services;
 
 namespace Tirax.Application.WireMocker.Components.Features.DesignServices;
 
 public sealed class SearchPanelViewModel : ViewModel
 {
+    readonly IDataStore dataStore;
+    readonly ReadOnlyObservableCollection<Service> services;
+    readonly ObservableAsPropertyHelper<bool> canNew;
     string serviceSearchText = string.Empty;
 
-    public SearchPanelViewModel(ShellViewModel shell, IScheduler scheduler, IViewModelFactory vmFactory) {
+    public SearchPanelViewModel(ShellViewModel shell, IScheduler scheduler, IViewModelFactory vmFactory, IDataStore dataStore) {
+        this.dataStore = dataStore;
         var normalized = this.WhenAnyValue(x => x.ServiceSearchText)
                              .Select(x => x.Trim())
                              .Select(s => string.IsNullOrWhiteSpace(s) ? null : s);
 
-        var canNew = normalized.Select(s => s is not null);
+        canNew = normalized.Select(s => s is not null).ToProperty(this, x => x.CanNew);
 
-        NewService = ReactiveCommand.CreateFromObservable<Unit, string>(
-            _ => normalized.Take(1)
-                           .Select(s => s ?? throw new ApplicationException("Race condition!"))
-                           .Do(title => {
-                                var vm = vmFactory.Create<AddServiceViewModel>(scheduler, title);
-                                shell.PushModal(vm);
-                                vm.Save.Where(r => r.IsSuccess).Subscribe(_ => ServiceSearchText = string.Empty);
-                            }),
-            canNew, scheduler);
+        var serviceChanges = new Subject<IChangeSet<Service>>();
+        var errorStream = new Subject<Error>();
+
+        NewService = ReactiveCommand.Create<string, Unit>(
+            title => {
+                var vm = vmFactory.Create<AddServiceViewModel>(scheduler, title);
+                shell.PushModal(vm);
+                vm.Save.Where(r => r.IsFail).Select(r => r.UnwrapError()).Subscribe(errorStream.OnNext);
+                vm.Save
+                  .Where(r => r.IsSuccess)
+                  .Do(_ => ServiceSearchText = string.Empty)
+                  .Select(r => r.Unwrap()).ToObservableChangeSet()
+                  .Subscribe(serviceChanges.OnNext);
+                return unit;
+            }, outputScheduler: scheduler);
+
+        var serviceSource = Observable.FromAsync(async () => await LoadAllServices().RunIO());
+
+        errorStream.Merge(serviceSource.Where(r => r.IsFail)
+                                       .Select(r => r.UnwrapError()))
+                   .Subscribe(e => shell.Notify((Severity.Error, e.ToString())));
+
+        var initSource = serviceSource.Where(r => r.IsSuccess)
+                                      .SelectMany(r => r.Unwrap())
+                                      .ToObservableChangeSet();
+
+        serviceChanges.Merge(initSource)
+                      .Do(_ => this.RaisePropertyChanging(nameof(Services)))
+                      .Bind(out services)
+                      .Subscribe(_ => this.RaisePropertyChanged(nameof(Services)));
     }
 
     public string ServiceSearchText
@@ -34,5 +67,14 @@ public sealed class SearchPanelViewModel : ViewModel
         set => this.RaiseAndSetIfChanged(ref serviceSearchText, value);
     }
 
-    public ReactiveCommand<Unit, string> NewService { get; }
+    public bool CanNew => canNew.Value;
+
+    public ReadOnlyObservableCollection<Service> Services => services;
+
+    public ReactiveCommand<string, Unit> NewService { get; }
+
+    OutcomeT<Asynchronous, Seq<Service>> LoadAllServices() =>
+        from itor in dataStore.GetServices()
+        from data in TryCatch(() => itor.ToArrayAsync())
+        select data.ToSeq();
 }
